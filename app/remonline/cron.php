db8eed30f9b2ebf338fcd6c3f8caf7abbe4ff77f
Roman Gonyukov <roman@bx24.store>
2020-05-18 13:35:26 +0700

    log off

--- app/remonline/cron.php
+++ app/remonline/cron.php
@@ -103,7 +103,7 @@ if (
 
     }
 
-    writeToLog(LOG_DIRECTORY . 'remonline/'.date('d-m-Y/H/') . 'new_client_' . date('H-i-s.') . 'txt', $logData, 'New client');
+    // writeToLog(LOG_DIRECTORY . 'remonline/'.date('d-m-Y/H/') . 'new_client_' . date('H-i-s.') . 'txt', $logData, 'New client');
 
 }
 
